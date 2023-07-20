9014b4b9cd3c452beaf2bd94b467d6fdb25a68d7
sarthak <sarthaksri044@gmail.com>
2023-07-20 16:17:55 +0530

    Local File Uploaded

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,14 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 
-export.cloudinaryConnect = () =>{
+require("dotenv").config();
+exports.cloudinaryConnect = () => {
     try{
-        cloudinary.config({
-            cloud_name:process.env.CLOUD_NAME,
-            api_key:process.env.API_KEY,
-            api_secret:process.env.API_SECRET,
-        })
+            cloudinary.config({
+                cloud_name:process.env.CLOUD_NAME,
+                api_key: process.env.API_KEY,
+                api_secret: process.env.API_SECRET,
+            })
     }
-    catch(error){
-        
+    catch(error) {
+        console.log(error);
     }
 }
---
+++ index.js
@@ -0,0 +1,29 @@
+//app create
+const express = require("express");
+const app = express();
+
+//PORt find krna h 
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+//middleware add krne h 
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());   //iss middleware ka use kr ke ham server par file upload krege
+                          // cloudinary media server pr upload krta hai
+
+//db se connect krna h 
+const db = require("./config/database");
+db.connect();
+
+//cloud se connect krna h 
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect(); 
+
+//api route mount krna h 
+const Upload = require("./routes/FileUpload");
+app.use('/api/v1/upload', Upload);
+
+//activate server
+app.listen(PORT, () => {
+    console.log(`App is running at ${PORT}`);
+})
